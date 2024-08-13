4931bf57bfbb974a7f7060fb1a564642cf297142
hamid_tahir <hamidtahir2000@gmail.com>
2024-08-13 17:05:06 +0500

    Add

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,7 +8,7 @@ export const errorHandler = (err, req, res, next) => {
   //Sometimes, error will have a 200 status code, even though there's an error.
   //for instance, if the error was manually thrown without setting a res.status:
   const errorCode = res.statusCode === 200 ? 500 : res.statusCode;
-
+  console.log(err);
   res.status(errorCode).json({
     message: err.message,
     stackTrace: process.env.NODE_ENV === "production" ? null : err.stack,
